4330f2b05de2246725a183c7945cbc7ec4f2c232
congcongcong <1414014544@qq.com>
2022-10-18 14:02:57 +0800

    提交文件

---
+++ RayTracingInNextWeek/RayTracing-Dev-main/main.cpp
@@ -0,0 +1,377 @@
+#include "camera.h"
+#include "rtweekend.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "moving_sphere.h"
+#include "material.h"
+#include "bvh.h"
+#include "surface_texture.h"
+#include "rtw_stb_image.h"
+#include "aarect.h"
+#include "box.h"
+#include "constant_medium.h"
+#include "pdf.h"
+#include<fstream>
+#include <iostream>
+#include <memory>
+
+vec3 ray_color(const ray& r, const vec3& background, const hittable& world, shared_ptr<hittable>& lights, int depth) {
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return vec3(0,0,0);
+
+    // If the ray hits nothing, return the background color.
+    if (!world.hit(r,0.001,infinity,rec))
+        return background;
+
+    ray scattered;
+    vec3 attenuation;
+    vec3 emitted = rec.mat_ptr->emitted(r, rec, rec.u, rec.v, rec.p);
+    double pdf_val;
+    vec3 albedo;
+
+    if (!rec.mat_ptr->scatter(r, rec, albedo, scattered, pdf_val))
+        return emitted;
+    auto p0 = make_shared<hittable_pdf>(lights, rec.p);
+    auto p1 = make_shared<cosine_pdf>(rec.normal);
+    mixture_pdf mixed_pdf(p0, p1);
+
+    scattered = ray(rec.p, mixed_pdf.generate(), r.time());
+    pdf_val = mixed_pdf.value(scattered.direction());
+
+    hittable_pdf light_pdf(lights, rec.p);
+    scattered = ray(rec.p, light_pdf.generate(), r.time());
+    pdf_val = light_pdf.value(scattered.direction());
+
+    return emitted
+        + albedo * rec.mat_ptr->scattering_pdf(r, rec, scattered)
+                 * ray_color(scattered, background, world, lights, depth - 1) / pdf_val;
+}
+
+hittable_list random_scene() {
+    hittable_list world;
+
+    //world.add(make_shared<sphere>(
+    //        vec3(0,-1000,0), 1000, make_shared<lambertian>(vec3(0.5, 0.5, 0.5))));
+
+    auto checker = make_shared<checker_texture>(
+            make_shared<constant_texture>(vec3(0.2, 0.3, 0.1)),
+            make_shared<constant_texture>(vec3(0.9, 0.9, 0.9))
+    );
+
+    world.add(make_shared<sphere>(vec3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
+
+    int i = 1;
+    for (int a = -10; a < 10; a++) {
+        for (int b = -10; b < 10; b++) {
+            auto choose_mat = random_double();
+            vec3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = vec3::random() * vec3::random();
+                    world.add(make_shared<moving_sphere>(
+                            center, center + vec3(0, random_double(0, 0.5), 0), 0.0, 1.0, 0.2,
+                            make_shared<lambertian>(albedo)));
+
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = vec3::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    world.add(
+                            make_shared<sphere>(center, 0.2, make_shared<metal>(albedo, fuzz)));
+                } else {
+                    // glass
+                    world.add(make_shared<sphere>(center, 0.2, make_shared<dielectric>(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(make_shared<sphere>(vec3(0, 1, 0), 1.0, make_shared<dielectric>(1.5)));
+    world.add(
+            make_shared<sphere>(vec3(-4, 1, 0), 1.0, make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+    world.add(
+            make_shared<sphere>(vec3(4, 1, 0), 1.0, make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+    return static_cast<hittable_list>(make_shared<bvh_node>(world,0,1));
+}
+
+
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(
+            make_shared<constant_texture>(vec3(0.2, 0.3, 0.1)),
+                        make_shared<constant_texture>(vec3(0.9, 0.9, 0.9)));
+
+    objects.add(make_shared<sphere>(vec3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(vec3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+    return objects;
+}
+
+hittable_list two_perlin_spheres() {
+    hittable_list objects;
+
+    auto pertex = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(vec3(0, -1000, 0), 1000, make_shared<lambertian>(pertex)));
+    objects.add(make_shared<sphere>(vec3(0, 2, 0), 2, make_shared<lambertian>(pertex)));
+
+    return objects;
+}
+
+hittable_list earth() {
+    int nx, ny, nn;
+    unsigned char* texture_data = stbi_load("vendor/stb/earthmap.jpg", &nx, &ny, &nn, 0);
+    if (!texture_data)
+        std::cerr << "ERROR: Could not load texture image file" << ".\n";
+
+    auto earth_surface =
+            make_shared<lambertian>(make_shared<image_texture>(texture_data, nx, ny));
+    auto globe = make_shared<sphere>(vec3(0,0,0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
+hittable_list simple_light() {
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(vec3(0,-1000, 0), 1000, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(vec3(0,2,0), 2, make_shared<lambertian>(pertext)));
+
+    auto difflight = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(4,4,4)));
+    objects.add(make_shared<sphere>(vec3(0,7,0), 2, difflight));
+    objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
+
+    return objects;
+}
+
+hittable_list cornell_box() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(15, 15, 15)));
+
+
+    objects.add(make_shared<flip_face>(make_shared<yz_rect>(0, 555, 0, 555, 555, green)));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(0, 555, 0, 555, 555, white)));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white)));
+
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(0, 0, 0), vec3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1,  15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(vec3(0,0,0), vec3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
+    return objects;
+}
+
+hittable_list cornell_smoke() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(7, 7, 7)));
+
+    objects.add(make_shared<flip_face>(make_shared<yz_rect>(0, 555, 0, 555, 555, green)));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(0, 555, 0, 555, 555, white)));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white)));
+
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(0,0,0), vec3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1,  15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+
+    shared_ptr<hittable> box2 = make_shared<box>(vec3(0,0,0), vec3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+
+    objects.add(
+            make_shared<constant_medium>(box1, 0.01, make_shared<constant_texture>(vec3(0,0,0))));
+    objects.add(
+            make_shared<constant_medium>(box2, 0.01, make_shared<constant_texture>(vec3(1,1,1))));
+
+    return objects;
+}
+
+hittable_list final_scene() {
+    hittable_list boxes1;
+    auto ground =
+            make_shared<lambertian>(make_shared<constant_texture>(vec3(0.48, 0.83, 0.53)));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i*w;
+            auto z0 = -1000.0 + j*w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1,101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(vec3(x0,y0,z0), vec3(x1,y1,z1), ground));
+        }
+    }
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(7, 7, 7)));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+    auto center1 = vec3(400, 400, 200);
+    auto center2 = center1 + vec3(30,0,0);
+    auto moving_sphere_material =
+            make_shared<lambertian>(make_shared<constant_texture>(vec3(0.7, 0.3, 0.1)));
+    objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<sphere>(vec3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    objects.add(make_shared<sphere>(
+            vec3(0, 150, 145), 50, make_shared<metal>(vec3(0.8, 0.8, 0.9), 10.0)
+    ));
+
+    auto boundary = make_shared<sphere>(vec3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(
+            boundary, 0.2, make_shared<constant_texture>(vec3(0.2, 0.4, 0.9))
+    ));
+    boundary = make_shared<sphere>(vec3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+    objects.add(make_shared<constant_medium>(
+            boundary, .0001, make_shared<constant_texture>(vec3(1,1,1))));
+
+    int nx, ny, nn;
+    auto tex_data = stbi_load("earthmap.jpg", &nx, &ny, &nn, 0);
+    auto emat = make_shared<lambertian>(make_shared<image_texture>(tex_data, nx, ny));
+    objects.add(make_shared<sphere>(vec3(400,200, 400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.1);
+    objects.add(make_shared<sphere>(vec3(220,280, 300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(vec3::random(0,165), 10, white));
+    }
+
+    objects.add(make_shared<translate>(
+            make_shared<rotate_y>(
+                    make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
+            vec3(-100,270,395)
+                )
+    );
+
+    return objects;
+}
+
+hittable_list cornell_box_plus() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(15, 15, 15)));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(213, 343, 227, 332, 554, light)));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(0,0,0), vec3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(vec3(0,0,0), vec3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
+
+    return objects;
+}
+
+
+int main() {
+
+    // Image
+    const int image_width = 200;
+    const int image_height = 100;
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+    const auto aspect_ratio = double(image_width) / image_height;
+
+    // World
+    //auto world = random_scene();
+    //auto world = two_spheres();
+    //auto world = two_perlin_spheres();
+    //auto world = earth();
+    auto world = simple_light();
+    //auto world = cornell_box();
+    //auto world = cornell_smoke();
+    //auto world = final_scene();
+    //auto world = cornell_box_plus();
+
+    const vec3 background(0, 0, 0);
+    shared_ptr<hittable> lights =
+            make_shared<xz_rect>(213, 343, 227, 332, 554, shared_ptr<material>());
+
+    // Camera
+    vec3 lookfrom(278, 278, -800);
+    vec3 lookat(278, 278, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 1.0;
+    auto vfov = 40.0;
+    auto time0 = 0.0;
+    auto time1 = 1.0;
+
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, time0, time1);
+
+    // Render
+	std::ofstream outfile("ResultCamera_sampleLight.ppm", std::ios_base::out);
+
+	outfile << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            vec3 color(0,0,0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / image_width;
+                auto v = (j + random_double()) / image_height;
+                ray r = cam.get_ray(u,v);
+                color += ray_color(r, background, world, lights, max_depth);
+            }
+			color /= float(samples_per_pixel);
+
+			color = vec3(sqrt(color[0]), sqrt(color[1]), sqrt(color[2]));
+
+			int ir = int(255.99 * color[0]);
+			int ig = int(255.99 * color[1]);
+			int ib = int(255.99 * color[2]);
+			outfile << ir << " " << ig << " " << ib << "\n";
+            //color.write_color(std::cout,samples_per_pixel);
+        }
+		std::cout << "6 " << std::endl;
+    }
+
+    std::cerr << "\nDone.\n";
+}
---
+++ RayTracingInNextWeek/RayTracing-Dev-main/material.h
@@ -0,0 +1,148 @@
+#ifndef RAYTRACING_DEV_MATERIAL_H
+#define RAYTRACING_DEV_MATERIAL_H
+
+#include "ray.h"
+#include "onb.h"
+#include "texture.h"
+#include "hittable.h"
+
+class material {
+public:
+    virtual vec3 emitted(const ray& r_in, const hit_record& rec, double u, double v, const vec3& p) const {
+        return vec3(0, 0, 0);
+    }
+
+    virtual bool scatter(
+            const ray& r_in,const hit_record& rec,vec3& attenuation,ray& scattered, double& pdf
+            ) const {
+        return false;
+    };
+
+    virtual double scattering_pdf (
+            const ray& r_in, const hit_record& rec, const ray& scattered
+            ) const {
+        return 0;
+    }
+};
+
+class lambertian : public material {
+public:
+    lambertian(const vec3& a) : albedo(make_shared<constant_texture>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
+
+    virtual bool scatter(
+            const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, double& pdf
+    ) const {
+        onb uvw;
+        uvw.build_from_w(rec.normal);
+        auto direction = uvw.local(random_cosine_direction());
+        vec3 scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction, r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        pdf = dot(uvw.w(), scattered.direction()) / pi;
+        return true;
+    }
+
+    double scattering_pdf(
+            const ray& r_in, const hit_record& rec, const ray& scattered
+            ) const {
+        auto cosine = dot(rec.normal, unit_vector(scattered.direction()));
+        return 0 ? 0 : cosine / pi;
+    }
+public:
+    shared_ptr<texture> albedo;
+};
+
+class metal : public material {
+public:
+    metal(const vec3& a,double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
+
+    virtual bool scatter(
+            const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+    ) const {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected + fuzz * random_in_sphere(), r_in.time());
+        attenuation = albedo;
+        return (dot(scattered.direction(), rec.normal) > 0);
+    }
+
+public:
+    vec3 albedo;
+    double fuzz;
+};
+
+class dielectric : public material {
+public:
+    dielectric(double ri) : ref_idx(ri) {}
+
+    virtual bool scatter (
+            const ray& r_in,const hit_record& rec,vec3& attenuation,ray& scattered
+    ) const {
+        double etai_over_etat;
+        attenuation = vec3(1.0, 1.0, 1.0);
+
+        if (rec.front_face) {
+            etai_over_etat = 1 / ref_idx;
+        } else {
+            etai_over_etat = ref_idx;
+        }
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = ffmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        if (etai_over_etat * sin_theta > 1.0) {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.p, reflected);
+            return true;
+        }
+
+        vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+        scattered = ray(rec.p, refracted, r_in.time());
+
+        return true;
+    }
+public:
+    double ref_idx;
+private:
+    static double reflectance(double cosine, double ref_idx) {
+        auto r0 = (1-ref_idx) / (1+ref_idx);
+        r0 = r0*r0;
+        return r0 + (1-r0)*pow((1 - cosine),5);
+    }
+};
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+
+    virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        return false;
+    }
+
+    virtual vec3 emitted(const ray& r_in, const hit_record& rec, double u, double v, const vec3& p) const {
+        return emit->value(u, v, p);
+    }
+
+public:
+    shared_ptr<texture> emit;
+};
+
+
+class isotropic : public material {
+public:
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    virtual bool scatter(
+            const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+    ) const  {
+        scattered = ray(rec.p, random_in_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+
+public:
+    shared_ptr<texture> albedo;
+};
+
+#endif //RAYTRACING_DEV_MATERIAL_H
